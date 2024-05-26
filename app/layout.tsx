98177abe6d6bc9f0e65074cb38ed6c1423d450fc
Tareq <tareqnmd@gmail.com>
2024-05-26 14:58:16 +0600

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body className={inter.className}>
-				<main className="app-content container mx-auto">
+				<main className="app-content container px-3 mx-auto">
 					<Menu />
 					{children}
 				</main>
